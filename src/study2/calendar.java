c67c77ffe2048ddc032dc53c374df5cf5b385982
Bigwon62 <taewon9262@gmail.com>
2020-06-15 02:17:04 +0900

    test

--- src/study2/calendar.java
+++ src/study2/calendar.java
@@ -1,5 +1,7 @@
 package study2;
 
+import java.util.Scanner;
+
 public class calendar {
     public static void main(String[] args) {
         System.out.println("일 월 화 수 목 금 토");
@@ -9,5 +11,13 @@ public class calendar {
         System.out.println("15 16 17 18 19 20 21");
         System.out.println("22 23 24 25 26 27 28");
 
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("월을 입력하세요");
+        int month = sc.nextInt();
+
+        int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        System.out.printf("%d월 %d일 까지 있습니다.\n", month, maxDays[month-1]);
+        sc.close();
     }
 }
