251f67948c9ca0f2049d33aa6b800e71e54e0556
Bigwon62 <taewon9262@gmail.com>
2020-06-26 11:06:19 +0900

    add weekday

--- src/study2/Prompt.java
+++ src/study2/Prompt.java
@@ -4,32 +4,61 @@ import java.util.Scanner;
 
 public class Prompt {
 
-    private final static String PROMPT = "cal> ";
+/*
+    @param week 요일명
+    @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
+*/
+
+    public int parseDay(String week) {
+        if (week.equals("su")){
+            return 0;
+        }else if(week.equals("mo")) {
+            return 1;
+        }else if(week.equals("tu")) {
+            return 2;
+        }else if(week.equals("we")) {
+            return 3;
+        }else if(week.equals("th")) {
+            return 4;
+        }else if(week.equals("fr")) {
+            return 5;
+        }else if(week.equals("sa")) {
+            return 6;
+        }else {
+            return 0;
+        }
+    }
 
     private void runPrompt() {
         Scanner sc = new Scanner(System.in);
         Calendar cal = new Calendar();
 
         int month = 1;
-        int year = 1;
+        int year = 2020;
+        int weekday = 0;
 
         while (true) {
-            System.out.println("년도를 입력하세요");
+            System.out.println("년도를 입력하세요(exit : -1)");
             System.out.print("YEAR> ");
             year = sc.nextInt();
-            System.out.println("월을 입력하세요");
-            System.out.print("MONTH> ");
-            month = sc.nextInt();
 
-            if (month == -1) {
+            if (year == -1){
                 break;
             }
 
-            if (month > 12) {
+            System.out.println("월을 입력하세요");
+            System.out.print("MONTH> ");
+            month = sc.nextInt();
+            System.out.println("첫째 날의 요일을 입력하세요");
+            String str_weekday = sc.next();
+            weekday = parseDay(str_weekday);
+
+            if (month > 12 || month < 1) {
+                System.out.println("잘못된 입력입니다.");
                 continue;
             }
 
-            cal.printCaledar(year, month);
+            cal.printCaledar(year, month, weekday);
         }
 
         System.out.println("끝내기");
